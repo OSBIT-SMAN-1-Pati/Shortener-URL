0d4e85d9e20a306b67ed613f46a9c760f4858047
YusufAqila <muhyusufa2402@gmail.com>
2023-09-03 18:33:09 +0700

    Fungsi Dasar

---
+++ pages/[key].tsx
@@ -0,0 +1,44 @@
+import { useRouter } from "next/router";
+import { useEffect } from 'react';
+
+export default function Key() {
+  const router = useRouter();
+  const { key } = router.query;
+  const jsonFile = [
+    {
+      key: "Youtube",
+      link: "https://youtube.com",
+    },
+    {
+      key: "facebook",
+      link: "https://facebook.com",
+    },
+    {
+      key: "instagram",
+      link: "https://instagram.com",
+    },
+  ];
+
+  const redirectURL = () => {
+    const foundKey = jsonFile.find(obj => obj.key === key);
+    if (foundKey) {
+      window.location.href = foundKey.link
+    } else {
+      router.push('/404');
+    }
+  }
+  
+  useEffect(() => {
+    if (key) {
+      redirectURL();
+    }
+    
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [key]);
+
+  return (
+    <>
+      <h1>Redirecting to {key}</h1>
+    </>
+  );
+}

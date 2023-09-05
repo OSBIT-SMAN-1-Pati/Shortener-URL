6ceaf729904e8d53645d7b708d1b15ab3da32780
Valmero Ahmadtofa <valism3ro@gmail.com>
2023-09-05 14:55:30 +0700

    Add timeout redirect hook➕

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,3 +1,7 @@
+
+import useTimeoutRedirect from "@/lib/useTimeoutRedirect"
+
 export default function ErrorPage() {
-    return <p>Not Found</p>
+    const {secondsRemaining} = useTimeoutRedirect("/",5)
+    return <p>Not Found! Will Redirect Within {secondsRemaining}</p>
 }

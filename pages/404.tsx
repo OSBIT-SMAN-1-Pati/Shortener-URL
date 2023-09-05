9b83579694ad520f40989e4b0911623207796c50
Valmero Ahmadtofa <valism3ro@gmail.com>
2023-09-05 21:37:18 +0700

    Revert "Add timeout redirect hook➕"
    This reverts commit 6ceaf729904e8d53645d7b708d1b15ab3da32780.

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,7 +1,3 @@
-
-import useTimeoutRedirect from "@/lib/useTimeoutRedirect"
-
 export default function ErrorPage() {
-    const {secondsRemaining} = useTimeoutRedirect("/",5)
-    return <p>Not Found! Will Redirect Within {secondsRemaining}</p>
+    return <p>Not Found</p>
 }

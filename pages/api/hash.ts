52e51f5e5a63e273a6d4e335acb11259b3c73fd9
Valmero Ahmadtofa <valism3ro@gmail.com>
2023-09-07 11:38:16 +0700

    Improve random short url

--- pages/api/hash.ts
+++ pages/api/hash.ts
@@ -4,17 +4,23 @@ type ResponseData = {
   message: string
   status?: number | string 
 }
- 
+function randomUrl(lenght:number){
+  let shortUrl = ''
+  const allowedChar = 'abcdefghijklmnopkrstufwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
+  for (let i=0;i<lenght;i++){
+    const randomChar = Math.floor(Math.random()* allowedChar.length)
+    shortUrl += allowedChar[randomChar]
+  }
+  return shortUrl
+}
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseData>
 ) {
   const data = JSON.parse(req.body)
-  console.log(data)
-  const salt = crypto.randomBytes(20).toString('hex')
   if(req.method !== "POST"){
     res.status(405).json({ message: 'Invalid' ,status:res.statusCode})
   } else
   
-  res.status(200).json({ message:`From ${data.long} to ${data.short || crypto.createHash('sha256').update(salt).digest('hex').slice(0,10)}`})
+  res.status(200).json({ message:`From ${data.long} to ${data.short || randomUrl(9)} `})
 }
